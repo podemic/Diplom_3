5f37dfcd19915f7cd1d078e53c2ba358c572b32b
Denis Polosukhin <denis.polosukhin@gmail.com>
2025-06-08 10:53:17 +0300

    little fixes, added requirements, fixed imports

--- scr/burgers_api.py
+++ scr/burgers_api.py
@@ -1,21 +1,21 @@
 import allure
 import requests
 
-from scr.constants import Url
+from scr.constants import BaseUrl
 
 
 class StellarBurgersAPI:
     @staticmethod
-    @allure.step('Запрос авторизации пользователя')
+    @allure.step('Запрос на авторизацию пользователя')
     def login_user(email, password):
-        url = f"{Url.BASE_URL}/auth/login"
+        url = f"{BaseUrl.BASE_URL}/auth/login"
         payload = {"email": email, "password": password}
         return requests.post(url, json=payload)
 
     @staticmethod
-    @allure.step("Запрос удаления пользователя")
+    @allure.step("Запрос на удаление пользователя")
     def delete_user(token, email, password, name):
-        url = f"{Url.BASE_URL}/auth/register"
+        url = f"{BaseUrl.BASE_URL}/auth/register"
         headers = {"Authorization": token}
         payload = {"email": email, "password": password, "name": name}
         return requests.delete(url, json=payload, headers=headers)
